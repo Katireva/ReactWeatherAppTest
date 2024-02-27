ae66ed501d299ae8c2b32a9e3d32e4d9320db069
NadiiaKat <nadiia.katyreva@infomentor.co.uk>
2024-02-27 22:13:00 +0000

    hardcode/completed design

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Inputs from "./components/Inputs";
 import TopButtons from "./components/TopButtons";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
@@ -12,6 +14,8 @@ function App() {
 
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -5,12 +5,12 @@ function TimeAndLocation() {
     <div>
       <div className="flex items-center justify-center my-6">
         <p className="text-white text-xl font-extralight">
-          Tuesday, 31 May 2021 | Local time: 12:46 PM
+          Tuesday, 24 February 2024 | Local time: 22:04 PM
         </p>
       </div>
 
       <div className="flex items-center justify-center my-3">
-        <p className="text-white text-3xl font-medium">Berlin, DE</p>
+        <p className="text-white text-3xl font-medium">Dnipro, UA</p>
       </div>
     </div>
   );
